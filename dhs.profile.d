5b3636a81baee072c8ec4b7a0d260eefe306b5d3
mdcb <mdcb@gemini.edu>
2008-06-02 16:00:59 +0000

    add rpm spec and profile.d basic setup
    git-svn-id: http://source.gemini.edu/software/dhs/trunk@2514 ee6ba543-07f8-4773-b02c-01d50e9d00ba

---
+++ dhs.profile.d
@@ -0,0 +1,5 @@
+export PATH=${PATH}:${GEMINI_TOP}/opt/dhs/bin
+export IMP_STARTUP=${GEMINI_TOP}/opt/dhs/var/imp_startup
+export DEFAULT_CONFIG_DIR=${GEMINI_TOP}/opt/dhs/var/default_config_dir
+export DHS_STAGING=${GEMINI_TOP}/opt/dhs/var/staging
+
