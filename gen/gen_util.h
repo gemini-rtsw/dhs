bb475ec6ce9f5e53878e768ba363d8f3daeb7154
vupadhya <vupadhya@gemini.edu>
2008-06-14 02:55:53 +0000

    Removing gen
    git-svn-id: http://source.gemini.edu/software/dhs/trunk@2824 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- gen/gen_util.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef _GEN_UTIL_H
-#define _GEN_UTIL_H
-
-/* Replacement for tempnam(3), which gcc complains about. (XXX allan) */
-char* gen_tempnam(const char *dir, const char *pfx);
-
-#endif /* _GEN_UTIL_H */
