d4aed39eec4d6f09ca9f9f0153a55bf6535f3c48
hstecher <hstecher@gemini.edu>
2022-03-14 21:59:29 +0000

    fixed file overwrite check to write file with tmp extension REL-3840
    git-svn-id: http://source.gemini.edu/software/dhs/trunk@70223 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- dhs/dhsData/put.C
+++ dhs/dhsData/put.C
@@ -458,10 +458,25 @@ void cDtsDhsPut::fileProcess
     }
     (void) fclose ( fp );
 
-    rename(path.c_str(), filePath);  // remove the .tmp
 
-    status.S_WROTE_FILE( status, path.c_str() );
 
+    //
+    // Check if the real name file exists
+    // if not rename tmp to real name
+    // otherwise keep it as tmp
+    // 
+
+    if (( fp = fopen( filePath,  "r" ) ) == NULL) 
+    {
+        rename(path.c_str(), filePath);  // remove the .tmp
+        status.S_WROTE_FILE( status, filePath );
+    }
+    else
+    {
+	printf("Error file already exists, saving as tmp: %s\n", filePath);
+	fclose(fp);
+        status.S_WROTE_FILE( status, path.c_str() );
+    }
 
 }
 
